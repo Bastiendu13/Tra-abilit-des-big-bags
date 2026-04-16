358f448e32bbeff041340a5cdbcb7edc0a48b098
Bastien Bigot <bstnbigot@gmail.com>
2026-04-16 11:16:52 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4159882322-5dc58",
+  "appId": "1:357655525306:web:032ec97d4d222e6f758f83",
+  "apiKey": "AIzaSyDkEXL3xDhEEQ28qtsW-o-O27R_49aNJXg",
+  "authDomain": "studio-4159882322-5dc58.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "357655525306"
+};
